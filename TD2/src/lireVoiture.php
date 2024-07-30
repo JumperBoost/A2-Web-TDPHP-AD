adffdba79424cf4ba603e18e733278ab094e4067
JumperBoost <milwenn59@sfr.fr>
2024-07-30 19:32:39 +0200

    Implémentation de l'exercice 7 Fin du 2ème TD

--- TD2/src/lireVoiture.php
+++ TD2/src/lireVoiture.php
@@ -1,18 +1,8 @@
 <?php
-require_once "ConnexionBaseDeDonnees.php";
-require_once "../../TD1/src/Voiture.php";
+require_once "Voiture.php";
 
-$pdo = ConnexionBaseDeDonnees::getPdo();
-$pdoStatement = $pdo->query("SELECT * FROM voiture");
-
-// Lire et afficher une voiture
-$voitureFormatTableau = $pdoStatement->fetch();
-var_dump($voitureFormatTableau);
-$voiture = new Voiture($voitureFormatTableau['immatriculationBaseDeDonnees'], $voitureFormatTableau['marqueBaseDeDonnees'], $voitureFormatTableau['couleurBaseDeDonnees'], $voitureFormatTableau['nbSiegesBaseDeDonnees']);
-echo $voiture;
-
-// Lire et afficher toutes les autres voitures
-foreach ($pdoStatement as $voitureFormatTableau2) {
-    $voiture2 = new Voiture($voitureFormatTableau2['immatriculationBaseDeDonnees'], $voitureFormatTableau2['marqueBaseDeDonnees'], $voitureFormatTableau2['couleurBaseDeDonnees'], $voitureFormatTableau2['nbSiegesBaseDeDonnees']);
-    echo "<p>$voiture2</p>";
+// Lire et afficher toutes les voitures
+$voitures = Voiture::getVoitures();
+foreach ($voitures as $voiture) {
+    echo "<p>$voiture</p>";
 }
