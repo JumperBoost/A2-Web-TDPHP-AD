8acb912e02e975bf4f435dd7a93c4a4fd0383d11
JumperBoost <milwenn59@sfr.fr>
2024-07-30 18:42:44 +0200

    Implémentation de l'exercice 5

--- TD2/src/ConnexionBaseDeDonnees.php
+++ TD2/src/ConnexionBaseDeDonnees.php
@@ -8,7 +8,13 @@ class ConnexionBaseDeDonnees {
 
     public function __construct() {
         $config = ConfigurationBaseDeDonnees::class;
-        $this->pdo = new PDO("mysql:host={$config::getNomHote()};port={$config::getPort()};dbname={$config::getNomBaseDeDonnees()}", $config::getLogin(), $config::getPassword());
+        // Connexion à la base de données
+        // Le dernier argument sert à ce que toutes les chaines de caractères en entrée et sortie de MySQL soit dans le codage UTF-8
+        $this->pdo = new PDO("mysql:host={$config::getNomHote()};port={$config::getPort()};dbname={$config::getNomBaseDeDonnees()}", $config::getLogin(), $config::getPassword(),
+            array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+        // On active le mode d'affichage des erreurs, et le lancement d'exception en cas d'erreur
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     public static function getPdo(): PDO {
