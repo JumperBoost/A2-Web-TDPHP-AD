483f60a93391ba684cd8dc9842378a45d32c9d68
JumperBoost <milwenn59@sfr.fr>
2024-07-30 18:38:39 +0200

    Implémentation de l'exercice 4

--- TD2/src/ConnexionBaseDeDonnees.php
+++ TD2/src/ConnexionBaseDeDonnees.php
@@ -2,6 +2,8 @@
 require_once "ConfigurationBaseDeDonnees.php";
 
 class ConnexionBaseDeDonnees {
+    private static ?ConnexionBaseDeDonnees $instance = null;
+
     private PDO $pdo;
 
     public function __construct() {
@@ -9,7 +11,19 @@ class ConnexionBaseDeDonnees {
         $this->pdo = new PDO("mysql:host={$config::getNomHote()};port={$config::getPort()};dbname={$config::getNomBaseDeDonnees()}", $config::getLogin(), $config::getPassword());
     }
 
-    public function getPdo(): PDO {
-        return $this->pdo;
+    public static function getPdo(): PDO {
+        return ConnexionBaseDeDonnees::getInstance()->pdo;
+    }
+
+    // getInstance s'assure que le constructeur ne sera
+    // appelé qu'une seule fois.
+    // L'unique instance crée est stockée dans l'attribut $instance
+    private static function getInstance() : ConnexionBaseDeDonnees {
+        // L'attribut statique $pdo s'obtient avec la syntaxe ConnexionBaseDeDonnees::$pdo
+        // au lieu de $this->pdo pour un attribut non statique
+        if (is_null(ConnexionBaseDeDonnees::$instance))
+            // Appel du constructeur
+            ConnexionBaseDeDonnees::$instance = new ConnexionBaseDeDonnees();
+        return ConnexionBaseDeDonnees::$instance;
     }
 }
