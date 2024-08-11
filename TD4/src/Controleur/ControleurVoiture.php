ba274b956c1439e56122e7c9ee0565f3ef2fd3f4
JumperBoost <milwenn59@sfr.fr>
2024-08-11 16:55:57 +0200

    Implémentation de l'exercice 7

--- TD4/src/Controleur/ControleurVoiture.php
+++ TD4/src/Controleur/ControleurVoiture.php
@@ -5,14 +5,19 @@ class ControleurVoiture {
     // Déclaration de type de retour void : la fonction ne retourne pas de valeur
     public static function afficherListe() : void {
         $voitures = ModeleVoiture::getVoitures(); //appel au modèle pour gérer la BD
-        require ('../vue/voiture/liste.php');  //"redirige" vers la vue
+        self::afficherVue("voiture/liste.php", ['voitures' => $voitures]);  //"redirige" vers la vue
     }
 
     public static function afficherDetail() : void {
         $voiture = ModeleVoiture::getVoitureParImmatriculation($_GET['immatriculation']);
         if(!is_null($voiture))
-            require ('../vue/voiture/detail.php');
-        else require ('../vue/voiture/erreur.php');
+            self::afficherVue("voiture/detail.php", ['voiture' => $voiture]);
+        else self::afficherVue("voiture/erreur.php");
+    }
+
+    private static function afficherVue(string $cheminVue, array $parametres = []) : void {
+        extract($parametres); // Crée des variables à partir du tableau $parametres
+        require "../vue/$cheminVue"; // Charge la vue
     }
 }
 ?>
